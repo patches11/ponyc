63a79facda961a49678756ec94de97f881d327aa
Patrick Brown <patrick.barry.brown@gmail.com>
2019-06-29 01:29:34 +0100

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-sudo apt install make gcc g++ git zlib1g-dev libncurses5-dev libssl-dev llvm llvm-dev libpcre2-dev
+sudo apt install make gcc g++ git zlib1g-dev libncurses5-dev libssl-dev llvm-3.9 llvm-3.9-dev libpcre2-dev
 
-export LLVM_CONFIG=/usr/lib/llvm/bin/llvm-config
+export LLVM_CONFIG=/usr/lib/llvm-3.9/bin/llvm-config
 
 make default_pic=true
