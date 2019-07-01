a6a19e57533c41740f59bf01da61fb43e66c836f
Patrick Brown <patrick.barry.brown@gmail.com>
2019-07-01 19:17:08 +0100

    update build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-sudo apt install make gcc g++ git zlib1g-dev libncurses5-dev libssl-dev llvm-3.9 llvm-3.9-dev libpcre2-dev
+sudo apt install make gcc g++ git zlib1g-dev libncurses5-dev libssl-dev llvm-3.9 llvm-3.9-dev libpcre2-dev cmake
 
 export LLVM_CONFIG=/usr/lib/llvm-3.9/bin/llvm-config
 
-make default_pic=true fpu=vfp arch=armv6zk cpu=arm1176jzf-s 
+make default_pic=true arch=armv6 tune=arm6 config=debug
