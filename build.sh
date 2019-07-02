41ed19f1924c6f7d94665506ceeca1c386448597
Patrick Brown <patrick.brown@blyncsy.com>
2019-07-02 12:01:25 -0600

    better build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-sudo apt install make gcc g++ git zlib1g-dev libncurses5-dev libssl-dev llvm-3.9 llvm-3.9-dev libpcre2-dev cmake
+
+if [ "$1" == "deps" ]; then
+    sudo apt install make gcc g++ git zlib1g-dev libncurses5-dev libssl-dev llvm-3.9 llvm-3.9-dev libpcre2-dev cmake
+fi
+
+if [ "$1" == "clean" ]; then
+  make clean
+fi
 
 export LLVM_CONFIG=/usr/lib/llvm-3.9/bin/llvm-config
 
