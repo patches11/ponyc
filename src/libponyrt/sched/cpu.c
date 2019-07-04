22dad6f1f28b8da939f001f6291e67ead952f106
Patrick Brown <patrick.brown@blyncsy.com>
2019-07-04 15:14:11 -0600

    disable test

--- src/libponyrt/sched/cpu.c
+++ src/libponyrt/sched/cpu.c
@@ -395,24 +395,24 @@ uint64_t ponyint_cpu_tick()
       return pmccntr << 6;
     }
   }
-#   elif defined ARMV6
+#   elif defined ARMV7
   // V6 is the earliest arch that has a standard cyclecount
   uint32_t pmccntr;
   uint32_t pmuseren;
   uint32_t pmcntenset;
 
   // Read the user mode perf monitor counter access permissions.
-  asm volatile ("MRC p15, 0, %0, c15, c9, 0" : "=r" (pmuseren));
+  asm volatile ("mrc p15, 0, %0, c15, c9, 0" : "=r" (pmuseren));
 
   // Allows reading perfmon counters for user mode code.
   if(pmuseren & 1)
   {
-    asm volatile ("MRC p15, 0, %0, c15, c12, 0 " : "=r" (pmcntenset));
+    asm volatile ("mrc p15, 0, %0, c15, c12, 0" : "=r" (pmcntenset));
 
     // Is it counting?
     if(pmcntenset & 1)
     {
-      asm volatile ("MRC p15, 0, %0, c15, c12, 1" : "=r" (pmccntr));
+      asm volatile ("mrc p15, 0, %0, c15, c12, 1" : "=r" (pmccntr));
 
       if ((pmcntenset >> 3) & 1) {
         // The counter is set up to count every 64th cycle
